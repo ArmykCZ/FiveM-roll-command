e37499c72b31a24d6946292d411cf8aa434f9740
ArmykCZ <matejjaros129@post.cz>
2025-02-11 11:01:58 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TvéJméno'
+description 'Jednoduchý /roll příkaz pro RP server'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
