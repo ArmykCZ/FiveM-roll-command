9609b0835ff31c641987ad9acc4ca1dd29655074
ArmykCZ <matejjaros129@post.cz>
2025-02-12 14:15:31 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
-RegisterCommand("roll", function(source, args, rawCommand)
-    local playerId = GetPlayerServerId(PlayerId())  -- Získá ID hráče
-    local roll = math.random(1, 6)  -- Náhodné číslo 1-6
+iRegisterCommand("roll", function(source, args, rawCommand)
+    local playerId = GetPlayerServerId(PlayerId()) 
+    local roll = math.random(1, 6) 
     TriggerServerEvent("sendRollMessage", roll, playerId)
 end, false)
